53c629fb5c5b1a25274a643a249291d8cf0dfdea
mdnestor <mdnest01@gmail.com>
2025-11-15 02:43:51 -0500

    move distance space assumption

--- basic_topology/Bounded.lean
+++ basic_topology/Bounded.lean
@@ -1,52 +1,52 @@
 import basic_topology.Metric
 import Mathlib.Data.Set.Finite.Basic
 
-variable {X Y D: Type*}
+variable {X Y D: Type*} [CompleteDistanceSpace D]
 
 -- diameter of a set
-noncomputable def diameter [CompleteDistanceSpace D] (d: X → X → D) (A: Set X): D :=
+noncomputable def diameter (d: X → X → D) (A: Set X): D :=
   sSup (⋃ (a ∈ A) (b ∈ A), {d a b})
 
 
-theorem diameter_monotone [CompleteDistanceSpace D] (d: X → X → D) {A B: Set X} (h: A ⊆ B): diameter d A ≤ diameter d B := by
+theorem diameter_monotone (d: X → X → D) {A B: Set X} (h: A ⊆ B): diameter d A ≤ diameter d B := by
   sorry
 
-theorem diameter_singleton [CompleteDistanceSpace D] {d: X → X → D} (hd: IsMetric d) (x: X): diameter d {x} = 0 := by
+theorem diameter_singleton {d: X → X → D} (hd: IsMetric d) (x: X): diameter d {x} = 0 := by
   simp [diameter]--, hd.dist_self_bot x]
   sorry -- missing import?
 
-example [CompleteDistanceSpace D] (d: X → X → D): diameter d ∅ = 0 := by
+example (d: X → X → D): diameter d ∅ = 0 := by
   simp [diameter]
   sorry
 
-example [CompleteDistanceSpace D] {d: X → X → D} (hd: IsMetric d) (x y: X): diameter d {x, y} = d x y := by
+example {d: X → X → D} (hd: IsMetric d) (x y: X): diameter d {x, y} = d x y := by
   simp [diameter]
   sorry
 
-example [CompleteDistanceSpace D] (d: X → X → D) (hd: IsMetric d) (x: X) (r: D): diameter d (openball d x r) ≤ 2 • r := by
+example (d: X → X → D) (hd: IsMetric d) (x: X) (r: D): diameter d (openball d x r) ≤ 2 • r := by
   simp [diameter]
   intros
   have := hd.triangle
   sorry
 
 -- a set is bounded if it has finite diameter.
-def bounded [CompleteDistanceSpace D] (d: X → X → D) (A: Set X): Prop :=
+def bounded (d: X → X → D) (A: Set X): Prop :=
   diameter d A < ⊤
 
 -- a set is boudned iff. it is contained in a ball of finite radius.
 -- maybe closed ball easier.
-theorem bounded_iff [CompleteDistanceSpace D] (d: X → X → D) (A: Set X): bounded d A ↔ ∃ x r, r < ⊤ ∧ A ⊆ openball d x r := by
+theorem bounded_iff (d: X → X → D) (A: Set X): bounded d A ↔ ∃ x r, r < ⊤ ∧ A ⊆ openball d x r := by
   sorry
 
-theorem bounded_subset [CompleteDistanceSpace D] (d: X → X → D) {A B: Set X} (h1: A ⊆ B) (h2: bounded d B): bounded d A := by
+theorem bounded_subset (d: X → X → D) {A B: Set X} (h1: A ⊆ B) (h2: bounded d B): bounded d A := by
   exact lt_of_le_of_lt (diameter_monotone _ h1) h2
 
 -- TODO if a finite family is all bounded their union is bounded.
-theorem bounded_finite_union [CompleteDistanceSpace D] (d: X → X → D) (F: Set (Set X)) (h1: Finite F) (h2: ∀ A ∈ F, bounded d A): bounded d (⋃₀ F) := by
+theorem bounded_finite_union (d: X → X → D) (F: Set (Set X)) (h1: Finite F) (h2: ∀ A ∈ F, bounded d A): bounded d (⋃₀ F) := by
   sorry
 
-def totally_bounded [CompleteDistanceSpace D] (d: X → X → D) (A: Set X): Prop :=
+def totally_bounded (d: X → X → D) (A: Set X): Prop :=
   ∀ ε, ⊥ < ε → ∃ C: Set X, Finite C ∧ A ⊆ ⋃ (x ∈ C), openball d x ε
 
-theorem totally_bounded_bounded [CompleteDistanceSpace D] {d: X → X → D} {A: Set X} (h: totally_bounded d A): bounded d A := by
+theorem totally_bounded_bounded {d: X → X → D} {A: Set X} (h: totally_bounded d A): bounded d A := by
   sorry
